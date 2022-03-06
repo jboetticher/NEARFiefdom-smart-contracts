628d9f44555f121e02f30c703201c488619f95e4
dogpool <83678936+dogpool@users.noreply.github.com>
2022-03-06 15:14:42 -0500

    upgrades1notdoneyet

---
+++ scripts/upgrade.js
@@ -0,0 +1,14 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+  // Deploying
+  const Box = await ethers.getContractFactory("Box");
+  const instance = await upgrades.deployProxy(Box, [42]);
+  await instance.deployed();
+
+  // Upgrading
+  const BoxV2 = await ethers.getContractFactory("BoxV2");
+  const upgraded = await upgrades.upgradeProxy(instance.address, BoxV2);
+}
+
+main();
