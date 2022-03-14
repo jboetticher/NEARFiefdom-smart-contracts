94bd02302459cad96cce1e9175b86f1546ceb0be
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-03-13 20:22:28 -0500

    added ganache to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,10 +18,17 @@ require("./tasks/transferFrom");
 require('dotenv').config();
 
 const AURORA_PRIVATE_KEY = process.env.AURORA_PRIVATE_KEY;
+const GANACHE_PRIVATE_KEY = process.env.GANACHE_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
+    ganache: {
+      url: "http://localhost:7545",
+      accounts: [`0x${GANACHE_PRIVATE_KEY}`],
+      chainId: 5777,
+      gasPrice: 50000000000
+    },
     testnet_aurora: {
       url: 'https://testnet.aurora.dev',
       accounts: [`0x${AURORA_PRIVATE_KEY}`],
